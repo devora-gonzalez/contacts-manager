559d1e3780095ac7c7f7ac19b5ad7ed7fab5aab2
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-04-10 17:56:12 -0400

    initial structure implemented

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String directory = "src/data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Path.of(directory);
+        Path dataFile = Path.of(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (!Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        List<String> myData = Arrays.asList("Luis|1234567", "Jenna|7654321");
+
+        Files.write(dataFile, myData);
+
+        String userName = "Kenneth";
+        String userNumber = "23456789";
+
+        Files.write(dataFile, (userName+"|"+userNumber).getBytes(), StandardOpenOption.APPEND);
+
+        System.out.println("Here's all the information in our file");
+
+        List<String> currentFileInfo = Files.readAllLines(dataFile);
+
+        for (String e : currentFileInfo){
+          String[] strings = e.split("\\|");
+            System.out.printf("%s | %s\n", strings[0], strings[1]);
+        }
+
+
+    }
+}
