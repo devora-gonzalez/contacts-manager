b6a9d31377ab72e1cb834f34b8f4c2634796770c
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-04-11 15:25:41 -0400

    delete functionality working

--- src/Main.java
+++ src/Main.java
@@ -32,20 +32,15 @@ public class Main {
         String userName;
         String userNumber;
 
-        List<String> currentFileInfo = Files.readAllLines(dataFile);
+        List<String> currentFileInfo;
 
-        System.out.println("Enter a name to delete contact information: ");
-        int userDelete = sc.nextInt();
-//        int userIndex = currentFileInfo.indexOf();
-        System.out.println(currentFileInfo.remove(userDelete));
-//        if (currentFileInfo.toString().toUpperCase().contains(userDelete.toUpperCase())) {
-//            currentFileInfo.indexOf(userDelete);
-//            System.out.println(currentFileInfo.indexOf(userDelete));
-//        }
+
+
 
         boolean userContinue;
 
         do {
+            currentFileInfo = Files.readAllLines(dataFile);
 
             System.out.println("1. View Contacts"); //display all
             System.out.println("2. Add a new contact"); // add new
@@ -60,12 +55,12 @@ public class Main {
 
             if (userChoice == 1) {
                 System.out.println("Here's all the contacts in our file:");
-                System.out.println("Name            | Phone Number");
+                System.out.printf("%-15s | %-8s | %n", "Name", "Phone Number");
                 System.out.println("------------------------------");
 
                 for (String e : currentFileInfo) {
                     String[] strings = e.split("\\|");
-                    String fmt = "%-15s | %-8s |%n";
+                    String fmt = "%-15s | %-12s |%n";
                     System.out.printf(fmt, strings[0], strings[1]);
                 }
             } else if (userChoice == 2) {
@@ -77,7 +72,9 @@ public class Main {
 
                 Files.write(dataFile, List.of(userName.trim() + "|" + userNumber.trim()), StandardOpenOption.APPEND);
 
-                System.out.println("Contact added successfully!");
+                System.out.println("Contact successfully added!!");
+
+
             } else if (userChoice == 3) {
                 System.out.print("Enter name to search: ");
 
@@ -91,17 +88,24 @@ public class Main {
                         }
                     }
                 } else {
-                    System.out.println("Contact does not exist");
+                    System.out.println("Contact does not exist.");
                 }
             } else if (userChoice == 4) {
-                // if statement to check if user exists
-                // true  - delete
-                //false - alert user (doesn't exist)
-//                System.out.println("Enter a name to delete contact information: ");
-//                String userDelete = sc.nextLine();
-//                if (currentFileInfo.toString().toUpperCase().contains(userDelete.toUpperCase())) {
-//                    currentFileInfo.remove(userDelete);
-//                }
+                System.out.println("Enter a name to delete contact information: ");
+                String userDelete = sc.nextLine();
+
+                if (currentFileInfo.toString().toUpperCase().contains(userDelete.toUpperCase())) {
+                    for (int i = 0; i < currentFileInfo.size(); i++) {
+                        String currentIndex = currentFileInfo.get(i);
+                        if (currentIndex.toUpperCase().contains(userDelete.toUpperCase())) {
+                            currentFileInfo.remove(i);
+                            Files.write(dataFile, currentFileInfo);
+                        }
+                    }
+                    System.out.println("Contact deleted.");
+                } else {
+                    System.out.println("Contact does not exist.");
+                }
             } else if (userChoice == 5) {
                 System.out.println("Exiting");
                 break;
@@ -111,7 +115,6 @@ public class Main {
 
             System.out.println("Would you like to return to menu? [Y/N]");
             userContinue = sc.next().equalsIgnoreCase("y");
-
         } while (userContinue);
 
     }
